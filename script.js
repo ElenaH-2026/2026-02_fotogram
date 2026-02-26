47595947020b702a71daa5341dfb88a9ffd23000
ElenaH-2026 <253562508+ElenaH-2026@users.noreply.github.com>
2026-02-26 11:48:50 +0100

    index.html, style.css, script.js and img added

---
+++ script.js
@@ -0,0 +1,9 @@
+const Dialog_Ref = document.getElementById("#DialogPhotoOverlay");
+
+function openDialogPhotoOverlay() {
+    Dialog_Ref.show();
+}
+
+function closeDialogPhotoOverlay() {
+    Dialog_Ref.close();
+}
