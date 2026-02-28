bf598ed5abd6ba783a45950e760d31852ea86078
ElenaH-2026 <253562508+ElenaH-2026@users.noreply.github.com>
2026-02-28 22:35:15 +0100

    index.html, style.css, photos.js modified

--- photos.js
+++ photos.js
@@ -1,3 +1,5 @@
+// Key must be identical to foldername AND section-id !!
+
 const All_Photos = {
     "elba":[
         {"year":2025, "copyright":"Michael Hiener", "description":"Elba - Cavo - Sonnenaufgang", "filename":"elba-cavo-sunrise-20251010.jpg"},
@@ -9,14 +11,10 @@ const All_Photos = {
     ],
 
     "usa-hawaii":[
-        {"year":2025, "copyright":"Michael Hiener", "description":"Elba - Cavo - Sonnenaufgang", "filename":"elba-cavo-sunrise-20251010.jpg"},
-        {"year":2025, "copyright":"Michael Hiener", "description":"Elba - Schloss in Cavo - Sonnenaufgang", "filename":"elba-cavo-castle-sunrise-20251006.jpg"},
-        {"year":2024, "copyright":"Michael Hiener", "description":"Neuseeland - Ninety Mile Beach", "filename":"nz-ninety-mile-beach-20240312.jpg"}
+        {"year":2017, "copyright":"Michael Hiener", "description":"USA - Death Valley", "filename":"usa-death-valley-2017.jpg"}
     ],
 
     "other":[
-        {"year":2025, "copyright":"Michael Hiener", "description":"Elba - Cavo - Sonnenaufgang", "filename":"elba-cavo-sunrise-20251010.jpg"},
-        {"year":2025, "copyright":"Michael Hiener", "description":"Elba - Schloss in Cavo - Sonnenaufgang", "filename":"elba-cavo-castle-sunrise-20251006.jpg"},
-        {"year":2024, "copyright":"Michael Hiener", "description":"Neuseeland - Ninety Mile Beach", "filename":"nz-ninety-mile-beach-20240312.jpg"}
+        {"year":2024, "copyright":"Elena Hiener", "description":"Österreich - Ehrwalder Almsee", "filename":"oesterreich-ehrwalder-almsee-202410.jpg"},
     ]
 }
--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 const DIALOG_REF = document.getElementById("#DialogPhotoOverlay");
 
-const PHOTOS_ELBA = document.getElementById("elba");
-const PHOTOS_NZ_FIJI = document.getElementById("nz-fiji");
-const PHOTOS_USA_HAWAII = document.getElementById("#usa-hawaii");
-const PHOTOS_OTHERS = document.getElementById("#others");
 
 function openDialogPhotoOverlay(no_bubbling) {
     DIALOG_REF.showModal();
@@ -15,23 +11,17 @@ function closeDialogPhotoOverlay() {
 }
 
 function initDisplayPhotoGallery() {
-    
     const REF_LIST = document.getElementsByClassName("ImageContainerPhotoGallery");
 
     for (let i = 0; i < REF_LIST.length; i++) {
-        if (REF_LIST[i].id == "nz-fiji") {
-            let constPhoto = PHOTOS_NZ_FIJI;
-            displayPhotoGallery(REF_LIST[i].id, constPhoto);
-        }
-        
-        
+        displayPhotoGallery(REF_LIST[i].id);
+        }   
     }
 
-}
-
-function displayPhotoGallery(foldername, constPhoto) {
+function displayPhotoGallery(foldername) {
     const PHOTOS = All_Photos[foldername];
-    let photo = constPhoto
+    let photo = document.getElementById(foldername);
+
     for (let i = 0; i < PHOTOS.length; i++) {
         let photo_filename = PHOTOS[i].filename;
         let photo_description = PHOTOS[i].description;
@@ -43,25 +33,3 @@ function displayPhotoGallery(foldername, constPhoto) {
                 alt="Foto: ${photo_description}"/>`;
     }
 }
-
-
-
-
-// --- Variante mit ID ---
-
-// function displayPhotoGallery(foldername) {
-//     const PHOTOS = All_Photos[foldername];
-//     let photo = document.getElementById("#elba");
-
-//     for (let i = 0; i < PHOTOS.length; i++) {
-//         let photo_filename = PHOTOS[i].filename;
-//         let photo_description = PHOTOS[i].description;
-            
-//         photo.innerHTML += `
-//             <img  onclick="openDialogPhotoOverlay(event)"
-//                 class="ImagePhotoGallery"
-//                 src="./photos/${foldername}/${photo_filename}" 
-//                 alt="Foto: ${photo_description}"/>`;
-//     }
-// }
-
