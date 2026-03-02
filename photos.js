5ff3f379a187d0f71c27a909d0c422fb51d5c032
ElenaH-2026 <253562508+ElenaH-2026@users.noreply.github.com>
2026-03-03 00:34:42 +0100

    style.css, script.js, photos.js modified

--- photos.js
+++ photos.js
@@ -1,49 +1,57 @@
+// folder:
+const ELBA = "Elba";
+const NZ_FIJI = "Neuseeland und Fiji";
+const USA_HAWAII = "USA und Hawaii";
+const OTHER = "Weitere Orte";
+
 // copyright:
 const MICHI = "Michael Hiener";
 const ELENA = "Elena Hiener";
+
+// Fotos komprimieren: https://www.irfanview.com/
 
 // Key must be identical to foldername AND container-id !!
 
 const All_PHOTOS = {
     "elba":[
-        {"year":2025, "copyright":MICHI, "description":"Elba - Cavo - Sonnenaufgang", "filename":"elba-20251010-cavo-sonnenaufgang.jpg"},
-        {"year":2025, "copyright":MICHI, "description":"Elba - Schloss in Cavo - Sonnenaufgang", "filename":"elba-20251006-cavo-schloss-sonnenaufgang.jpg"},
-        {"year":2024, "copyright":MICHI, "description":"Elba - Sonnenaufgang", "filename":"elba-20240930-cavo-sonnenaufgang.jpg"},
-        {"year":2024, "copyright":MICHI, "description":"Elba - Cavo - Hafen", "filename":"elba-20240919-cavo-hafen.jpg"},
+        {"folder":ELBA, "year":2025, "copyright":MICHI, "description":"Elba - Cavo - Sonnenaufgang", "filename":"elba-20251010-cavo-sonnenaufgang.jpg"},
+        {"folder":ELBA, "year":2025, "copyright":MICHI, "description":"Elba - Schloss in Cavo - Sonnenaufgang", "filename":"elba-20251006-cavo-schloss-sonnenaufgang.jpg"},
+        {"folder":ELBA, "year":2024, "copyright":MICHI, "description":"Elba - Sonnenaufgang", "filename":"elba-20240930-cavo-sonnenaufgang.jpg"},
+        {"folder":ELBA, "year":2024, "copyright":MICHI, "description":"Elba - Cavo - Hafen", "filename":"elba-20240919-cavo-hafen.jpg"},
     ],
 
     "nz-fiji":[
-        {"year":2024, "copyright":MICHI, "description":"Neuseeland - Lake Tekapo - Camper", "filename":"nz-20240213-lake-tekapo-camper.jpg"},
-        {"year":2024, "copyright":MICHI, "description":"Neuseeland - Lake Tekapo", "filename":"nz-20240214-lake-tekapo.jpg"},
-        {"year":2024, "copyright":MICHI, "description":"Neuseeland - Papatowai", "filename":"nz-20240216-papatowai.jpg"},
-        {"year":2024, "copyright":MICHI, "description":"Neuseeland - Manapouri - Regenbogen", "filename":"nz-20240219-manapouri-regenbogen.jpg"},
-        {"year":2024, "copyright":MICHI, "description":"Neuseeland - Lake Manapouri", "filename":"nz-20240220-lake-manapouri.jpg"},
-        {"year":2024, "copyright":MICHI, "description":"Neuseeland - Doubtful Sound", "filename":"nz-20240220-doubtful-sound.jpg"},
-        {"year":2024, "copyright":MICHI, "description":"Neuseeland - Doubtful Sound - Nee Islets", "filename":"nz-20240220-doubtful-sound-nee-islets.jpg"},
-        {"year":2024, "copyright":MICHI, "description":"Neuseeland - Lake Hāwea", "filename":"nz-20240221-lake-hawea.jpg"},
-        {"year":2024, "copyright":MICHI, "description":"Neuseeland - Archway Rocks", "filename":"nz-20240228-archway-rocks.jpg"},
-        {"year":2024, "copyright":MICHI, "description":"Neuseeland - Wharariki Beach", "filename":"nz-20240228-wharariki-beach.jpg"},
-        {"year":2024, "copyright":MICHI, "description":"Neuseeland - Tauhara Forest", "filename":"nz-20240306-tauhara-forest.jpg"},
-        {"year":2024, "copyright":MICHI, "description":"Neuseeland - Taupiri - The Last Kauri Tree", "filename":"nz-20240308-taupiri-the-last-kauri-tree.jpg"},
-        {"year":2024, "copyright":MICHI, "description":"Neuseeland - Ninety Mile Beach", "filename":"nz-20240312-ninety-mile-beach.jpg"},
-        {"year":2024, "copyright":MICHI, "description":"Neuseeland - Ninety Mile Beach", "filename":"nz-20240312-ninety-mile-beach-panorama.jpg"},
-        {"year":2024, "copyright":MICHI, "description":"Neuseeland - Koutu Point Sonnenuntergang", "filename":"nz-20240314-koutu-point-sonnenuntergang.jpg"},
-        {"year":2024, "copyright":MICHI, "description":"Neuseeland - Hot Water Beach", "filename":"nz-20240316-hot-water-beach.jpg"},
-        {"year":2024, "copyright":MICHI, "description":"Neuseeland - Rotorua", "filename":"nz-20240319-rotorua.jpg"},
-        {"year":2024, "copyright":MICHI, "description":"Neuseeland - Orere Point - Wolkenhimmel", "filename":"nz-20240319-orere-point-wolkenhimmel.jpg"},
-        {"year":2024, "copyright":MICHI, "description":"Fiji - Barefoot Manta Island Resort - Sunrise Beach", "filename":"fiji-20240324-barefoot-manta-sunrise.jpg"},
-        {"year":2024, "copyright":MICHI, "description":"Fiji - Barefoot Manta Island Resort - Sunset Beach", "filename":"fiji-20240325-barefoot-manta-sunset.jpg"},
-        {"year":2024, "copyright":MICHI, "description":"Fiji - Namada - Strand in der Nacht", "filename":"fiji-20240401-namada-strand-nachts.jpg"},
-        {"year":2024, "copyright":MICHI, "description":"Fiji - Namada - Palme", "filename":"fiji-20240402-namada-palme.jpg"},
+        {"folder":NZ_FIJI, "year":2024, "copyright":MICHI, "description":"Neuseeland - Lake Tekapo - Camper", "filename":"nz-20240213-lake-tekapo-camper.jpg"},
+        {"folder":NZ_FIJI, "year":2024, "copyright":MICHI, "description":"Neuseeland - Lake Tekapo", "filename":"nz-20240214-lake-tekapo.jpg"},
+        {"folder":NZ_FIJI, "year":2024, "copyright":MICHI, "description":"Neuseeland - Papatowai", "filename":"nz-20240216-papatowai.jpg"},
+        {"folder":NZ_FIJI, "year":2024, "copyright":MICHI, "description":"Neuseeland - Manapouri - Regenbogen", "filename":"nz-20240219-manapouri-regenbogen.jpg"},
+        {"folder":NZ_FIJI, "year":2024, "copyright":MICHI, "description":"Neuseeland - Lake Manapouri", "filename":"nz-20240220-lake-manapouri.jpg"},
+        {"folder":NZ_FIJI, "year":2024, "copyright":MICHI, "description":"Neuseeland - Doubtful Sound", "filename":"nz-20240220-doubtful-sound.jpg"},
+        {"folder":NZ_FIJI, "year":2024, "copyright":MICHI, "description":"Neuseeland - Doubtful Sound - Nee Islets", "filename":"nz-20240220-doubtful-sound-nee-islets.jpg"},
+        {"folder":NZ_FIJI, "year":2024, "copyright":MICHI, "description":"Neuseeland - Lake Hāwea", "filename":"nz-20240221-lake-hawea.jpg"},
+        {"folder":NZ_FIJI, "year":2024, "copyright":MICHI, "description":"Neuseeland - Archway Rocks", "filename":"nz-20240228-archway-rocks.jpg"},
+        {"folder":NZ_FIJI, "year":2024, "copyright":MICHI, "description":"Neuseeland - Wharariki Beach", "filename":"nz-20240228-wharariki-beach.jpg"},
+        {"folder":NZ_FIJI, "year":2024, "copyright":MICHI, "description":"Neuseeland - Tauhara Forest", "filename":"nz-20240306-tauhara-forest.jpg"},
+        {"folder":NZ_FIJI, "year":2024, "copyright":MICHI, "description":"Neuseeland - Taupiri - The Last Kauri Tree", "filename":"nz-20240308-taupiri-the-last-kauri-tree.jpg"},
+        {"folder":NZ_FIJI, "year":2024, "copyright":MICHI, "description":"Neuseeland - Ninety Mile Beach", "filename":"nz-20240312-ninety-mile-beach.jpg"},
+        {"folder":NZ_FIJI, "year":2024, "copyright":MICHI, "description":"Neuseeland - Ninety Mile Beach", "filename":"nz-20240312-ninety-mile-beach-panorama.jpg"},
+        {"folder":NZ_FIJI, "year":2024, "copyright":MICHI, "description":"Neuseeland - Koutu Point Sonnenuntergang", "filename":"nz-20240314-koutu-point-sonnenuntergang.jpg"},
+        {"folder":NZ_FIJI, "year":2024, "copyright":MICHI, "description":"Neuseeland - Nacht am Hot Water Beach", "filename":"nz-20240316-hot-water-beach.jpg"},
+        {"folder":NZ_FIJI, "year":2024, "copyright":MICHI, "description":"Neuseeland - Rotorua", "filename":"nz-20240319-rotorua.jpg"},
+        {"folder":NZ_FIJI, "year":2024, "copyright":MICHI, "description":"Neuseeland - Orere Point - Wolkenhimmel", "filename":"nz-20240319-orere-point-wolkenhimmel.jpg"},
+        {"folder":NZ_FIJI, "year":2024, "copyright":MICHI, "description":"Fiji - Barefoot Manta Island Resort - Sunrise Beach", "filename":"fiji-20240324-barefoot-manta-sunrise.jpg"},
+        {"folder":NZ_FIJI, "year":2024, "copyright":MICHI, "description":"Fiji - Barefoot Manta Island Resort - Sunset Beach", "filename":"fiji-20240325-barefoot-manta-sunset.jpg"},
+        {"folder":NZ_FIJI, "year":2024, "copyright":MICHI, "description":"Fiji - Namada - Strand in der Nacht", "filename":"fiji-20240401-namada-strand-nachts.jpg"},
+        {"folder":NZ_FIJI, "year":2024, "copyright":MICHI, "description":"Fiji - Namada - Palme", "filename":"fiji-20240402-namada-palme.jpg"},
     ],
 
     "usa-hawaii":[
-        {"year":2017, "copyright":MICHI, "description":"USA - Death Valley", "filename":"usa-2017-death-valley.jpg"},
+        {"folder":USA_HAWAII, "year":2017, "copyright":MICHI, "description":"USA - Death Valley", "filename":"usa-2017-death-valley.jpg"},
     ],
 
     "other":[
-        {"year":2023, "copyright":MICHI, "description":"Griechenland - Kalamos - Strand", "filename":"griechenland-20230707-kalamos-strand.jpg"},
-        {"year":2024, "copyright":ELENA, "description":"Österreich - Ehrwalder Almsee", "filename":"oesterreich-202410-ehrwalder-almsee.jpg"},
-        {"year":2024, "copyright":MICHI, "description":"Italien - Terni - Cascate delle Marmore", "filename":"italien-20240524-terni-cascate-delle-marmore.jpg"},
+        {"folder":OTHER,"year":2023, "copyright":MICHI, "description":"Griechenland - Kalamos - Strand", "filename":"griechenland-20230707-kalamos-strand.jpg"},
+        {"folder":OTHER,"year":2024, "copyright":ELENA, "description":"Österreich - Ehrwalder Almsee", "filename":"oesterreich-202410-ehrwalder-almsee.jpg"},
+        {"folder":OTHER,"year":2024, "copyright":MICHI, "description":"Italien - Terni - Cascate delle Marmore", "filename":"italien-20240524-terni-cascate-delle-marmore.jpg"},
     ]
 }
--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const DIALOG_REF = document.getElementById("#DialogPhotoOverlay");
 const FOLDER_LIST = document.getElementsByClassName("ImageContainerPhotoGallery");
-let folder = "";
-let photos_per_folder = All_PHOTOS[folder];
-let photo = "";
+let photos_thumbnail = "";
 let photo_overlay = document.getElementById("#PhotoOverlay");
 
-function openDialogPhotoOverlay(event_no_bubbling) {
+function openDialogPhotoOverlay(event_no_bubbling, photo_src, photo_alt, photo_description, photo_copyright, photo_nr) {
     event_no_bubbling.stopPropagation();
-    DIALOG_REF.showModal();   
+    DIALOG_REF.showModal();
+
+    photo_overlay.innerHTML = displayPhotoOverlay(photo_src, photo_alt, photo_description, photo_copyright, photo_nr);
 }
 
 // Warum schließt sich das Fenster, auch wenn ich auf den Header/Footer oder das Dialogfenster selbst klicke??
@@ -18,36 +18,38 @@ function closeDialogPhotoOverlay() {
 function initDisplayPhotoGallery() {
 
     for (let i = 0; i < FOLDER_LIST.length; i++) {
-        folder = FOLDER_LIST[i].id;
-        photo = document.getElementById(folder);
+        let folder = FOLDER_LIST[i].id;
+        photos_thumbnail = document.getElementById(folder);
         getPhoto(folder);
         }   
     }
 
 function getPhoto(foldername) {
-    photos_per_folder = All_PHOTOS[foldername];
+    let photos_per_folder = All_PHOTOS[foldername];
 
     for (let i = 0; i < photos_per_folder.length; i++) {
+        let photo_id = foldername + "-" + (i+1);
         let photo_src = "./photos/" + foldername + "/" + photos_per_folder[i].filename;
         let photo_alt = "Foto: " + photos_per_folder[i].description;
         let photo_description = photos_per_folder[i].description;
         let photo_copyright = "&#169 " + photos_per_folder[i].copyright;
-            
-        photo.innerHTML += displayPhotoThumbnail(photo_src, photo_alt);
-        photo_overlay.innerHTML = displayPhotoOverlay(photo_description,photo_src, photo_alt, photo_copyright);
+        let photo_nr = photos_per_folder[i].folder + ": " + (i+1) + " von " + photos_per_folder.length;
+        
+        photos_thumbnail.innerHTML += displayPhotoThumbnail(photo_id, photo_src, photo_alt, photo_description, photo_copyright, photo_nr);
     }
 }
 
-function displayPhotoThumbnail(photo_src, photo_alt) {
+function displayPhotoThumbnail(photo_id, photo_src, photo_alt, photo_description, photo_copyright, photo_nr) {
     return `
-        <img onclick="openDialogPhotoOverlay(event)"
+        <img onclick="openDialogPhotoOverlay(event, '${photo_src}', '${photo_alt}', '${photo_description}', '${photo_copyright}', '${photo_nr}')"
             class="ImagePhotoGallery"
+            id=${photo_id}
             src=${photo_src}
             alt=${photo_alt}
         />`;
 }
 
-function displayPhotoOverlay(photo_description, photo_src, photo_alt, photo_copyright) {
+function displayPhotoOverlay(photo_src, photo_alt, photo_description, photo_copyright, photo_nr) {
     return `
         <header>
             <h3>${photo_description}</h3>
@@ -63,7 +65,7 @@ function displayPhotoOverlay(photo_description, photo_src, photo_alt, photo_copy
         </section>
         <footer>
             <button>(<=)</button>
-            <span>y/x</span>
+            <span>${photo_nr}</span>
             <button>(=>)</button>
         </footer>`;
 }
